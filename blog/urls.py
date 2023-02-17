26b32dda9d86aa754f4330634e2a2463d7017f2c
azez0 <nanquery@gmail.com>
2023-02-17 18:40:13 +0100

    set url path for single post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='blog-starting-page'),
     path('posts/', views.posts, name='posts-page'),
-    path('posts/<int:post_id>', views.post_detail, name='post-detail-page'),
+    path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
 ]
