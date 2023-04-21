67b15ef57a7e4570d479e36e456e439342f825cd
azez0 <nanquery@gmail.com>
2023-04-21 18:40:46 +0200

    configure admin panel base on models changes

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -2,17 +2,20 @@ from django.contrib import admin
 from .models import Post, Category, Tag, Comment, Author, Subscriber
 
 
-# @admin.register(Post)
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_posted')
     list_filter = ('date_posted', 'categories', 'tags')
     search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    # Dodanie prepopulated_fields wykorzystuje mechanizm slugowania, który bazuje na polu 'title'
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ('name', 'created_by', 'date_created')
+    search_fields = ('name', 'created_by__username')
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Tag)
