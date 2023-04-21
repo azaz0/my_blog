050f896c36bf48d54fac82f8dee060f11818674e
azez0 <nanquery@gmail.com>
2023-04-21 18:47:04 +0200

    add urls for views

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,21 +1,15 @@
-"""my_blog URL Configuration
+from django.urls import include, path
+from rest_framework import routers
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+router = routers.DefaultRouter()
+router.register(r'authors', views.AuthorViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'subscribers', views.SubscriberViewSet)
+router.register(r'tags', views.TagViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
